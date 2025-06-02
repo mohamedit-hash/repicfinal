d905245b056dc57d63308d28c445095376ddfa77
mohamedit-hash <mohamed.a.allouache@gmail.com>
2025-06-02 20:54:58 +0100

    Update message.route.js

--- backend/scr/routes/message.route.js
+++ backend/scr/routes/message.route.js
@@ -3,9 +3,7 @@ import { protect as protectRouter } from "../middlewares/auth.js";
 import { getUsersForSidebar, getMessages, sendMessages } from "../controller/message.controller.js";
 
 const router = Router();
-// hado l users li ykouno fl left side
 router.get("/users",protectRouter,getUsersForSidebar)
-//hadi t3 les msg ki yjio tht b3dahoum fl conv
 router.get("/:id", protectRouter,getMessages)
 
 router.post("/send/:id",protectRouter,sendMessages)
